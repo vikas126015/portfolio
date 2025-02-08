9cc4f895cbbe802f1347715c0536dde7530c7ac5
vikas D <vikas126015@gmail.com>
2025-02-08 19:32:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import *
-app=Flask(__name__)
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
 @app.route('/')
 def sample():
     return render_template('index.html')
-if __name__=='__main__':
+
+@app.route('/static')
+def download_resume():
+    return send_from_directory('static', 'resume.pdf', as_attachment=True)
+
+if __name__ == '__main__':
     app.run(debug=True)
